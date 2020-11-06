771f78754d39694b6b87f7f42524740f282fb661
muz4151 <murtazau12@hotmail.com>
2020-11-06 11:34:14 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -30,9 +30,9 @@ def follower_count(user):
 api = create_api()
 
 while True:
-    user = api.get_user('Ameen91741779')
-    api.update_profile(name=f'ameen|{follower_count(user)} Followers')
-    print(f'Updating Twitter Name : ameen|{follower_count(user)} Followers')
+    user = api.get_user('MurtazaUdaipur2')
+    api.update_profile(name=f'Random123|{follower_count(user)} Followers')
+    print(f'Updating Twitter Name : Random123|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
   
